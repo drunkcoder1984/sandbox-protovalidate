fc9ae3e8f34bd1345bdde0fc90070f59a2f42ef4
drunkcoder1984 <drunkcoder1984@gmail.com>
2024-05-22 11:33:48 +0900

    buf config v1の実装追加

---
+++ 01_buf_confv1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	userpb "buf-confv1/gen/go/proto/user"
+
+	"github.com/bufbuild/protovalidate-go"
+)
+
+func main() {
+	v, err := protovalidate.New()
+	if err != nil {
+		log.Fatalln("Error protovalidate", err)
+	}
+
+	// Nameのバリデーション5文字以上なのでerrorになる
+	user := &userpb.User{Name: "hoge"}
+	if err := v.Validate(user); err != nil {
+		fmt.Printf("Name: %s error: %v\n\n", user.Name, err)
+	} else {
+		fmt.Printf("Name: %s ok\n\n", user.Name)
+	}
+
+	// Nameのバリデーション5文字以上なのでokになる
+	user = &userpb.User{Name: "hogehoge"}
+	if err := v.Validate(user); err != nil {
+		fmt.Printf("Name: %s error: %v\n\n", user.Name, err)
+	} else {
+		fmt.Printf("Name: %s ok\n\n", user.Name)
+	}
+}
